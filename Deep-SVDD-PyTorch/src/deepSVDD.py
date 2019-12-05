23fb54dee48364333d17563f7c9569d2cc2cad39
Guillaume Vray <guillaume.vray@epfl.ch>
2019-12-05 16:12:23 +0100

    Add Deep SVDD

---
+++ Deep-SVDD-PyTorch/src/deepSVDD.py
@@ -0,0 +1,100 @@
+import json
+import torch
+
+from base.base_dataset import BaseADDataset
+from networks.main import build_network
+from optim.deepSVDD_trainer import DeepSVDDTrainer
+
+
+class DeepSVDD(object):
+    """A class for the Deep SVDD method.
+
+    Attributes:
+        objective: A string specifying the Deep SVDD objective (either 'one-class' or 'soft-boundary').
+        nu: Deep SVDD hyperparameter nu (must be 0 < nu <= 1).
+        R: Hypersphere radius R.
+        c: Hypersphere center c.
+        net_name: A string indicating the name of the neural network to use.
+        net: The neural network \phi.
+        ae_net: The autoencoder network corresponding to \phi for network weights pretraining.
+        trainer: DeepSVDDTrainer to train a Deep SVDD model.
+        optimizer_name: A string indicating the optimizer to use for training the Deep SVDD network.
+        ae_trainer: AETrainer to train an autoencoder in pretraining.
+        ae_optimizer_name: A string indicating the optimizer to use for pretraining the autoencoder.
+        results: A dictionary to save the results.
+    """
+
+    def __init__(self, objective: str = 'one-class', nu: float = 0.1):
+        """Inits DeepSVDD with one of the two objectives and hyperparameter nu."""
+
+        assert objective in ('one-class', 'soft-boundary'), "Objective must be either 'one-class' or 'soft-boundary'."
+        self.objective = objective
+        assert (0 < nu) & (nu <= 1), "For hyperparameter nu, it must hold: 0 < nu <= 1."
+        self.nu = nu
+        self.R = 0.0  # hypersphere radius R
+        self.c = None  # hypersphere center c
+
+        self.net_name = None
+        self.net = None  # neural network \phi
+
+        self.trainer = None
+        self.optimizer_name = None
+
+        self.results = {
+            'train_time': None,
+            'test_auc': None,
+            'test_time': None,
+            'test_scores': None,
+        }
+
+    def set_network(self, net_name, rep_dim):
+        """Builds the neural network \phi."""
+        self.net_name = net_name
+        self.net = build_network(net_name, rep_dim)
+
+    def train(self, cfg, dataset: BaseADDataset, optimizer_name: str = 'adam', lr: float = 0.001, n_epochs: int = 50,
+              lr_milestones: tuple = (), batch_size: int = 128, weight_decay: float = 1e-6, device: str = 'cuda',
+              n_jobs_dataloader: int = 0):
+        """Trains the Deep SVDD model on the training data."""
+
+        self.optimizer_name = optimizer_name
+        self.trainer = DeepSVDDTrainer(self.objective, self.R, self.c, self.nu, optimizer_name, lr=lr,
+                                       n_epochs=n_epochs, lr_milestones=lr_milestones, batch_size=batch_size, weight_decay=weight_decay, device=device, n_jobs_dataloader=n_jobs_dataloader)
+        # Get the model
+        self.net = self.trainer.train(self, cfg, dataset, self.net)
+        self.results['train_time'] = self.trainer.train_time
+
+    def test(self, dataset: BaseADDataset, device: str = 'cuda', n_jobs_dataloader: int = 0, batch_size: int = 128):
+        """Tests the Deep SVDD model on the test data."""
+
+        if self.trainer is None:
+            self.trainer = DeepSVDDTrainer(self.objective, self.R, self.c, self.nu,                 batch_size=batch_size, device=device, n_jobs_dataloader=n_jobs_dataloader)
+
+        self.trainer.test(dataset, self.net)
+        # Get results
+        self.results['test_auc'] = self.trainer.test_auc
+        self.results['test_time'] = self.trainer.test_time
+        self.results['test_scores'] = self.trainer.test_scores
+
+    def save_model(self, export_model):
+        """Save Deep SVDD model to export_model."""
+
+        net_dict = self.net.state_dict()
+        print(self.R, self.c)
+        torch.save({'R': self.R,
+                    'c': self.c,
+                    'net_dict': net_dict}, export_model)
+
+    def load_model(self, model_path):
+        """Load Deep SVDD model from model_path."""
+
+        model_dict = torch.load(model_path)
+
+        self.R = model_dict['R']
+        self.c = model_dict['c']
+        self.net.load_state_dict(model_dict['net_dict'])
+
+    def save_results(self, export_json):
+        """Save results dict to a JSON-file."""
+        with open(export_json, 'w') as fp:
+            json.dump(self.results, fp)
---
+++ Deep-SVDD-PyTorch/src/networks/main.py
@@ -0,0 +1,30 @@
+from .mnist_LeNet import MNIST_LeNet_Autoencoder
+from .cifar10_LeNet import CIFAR10_LeNet_Autoencoder
+from .cifar10_LeNet_elu import CIFAR10_LeNet_ELU_Autoencoder
+from .resnet18 import ResNet18Autoencoder
+from .fmnist_LeNet import FMNIST_LeNet_Autoencoder
+
+def build_network(net_name, rep_dim):
+    """Builds the corresponding autoencoder network."""
+
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU', 'CXR_resnet18', 'fmnist_LeNet')
+    assert net_name in implemented_networks
+
+    ae_net = None
+
+    if net_name == 'mnist_LeNet':
+        ae_net = MNIST_LeNet_Autoencoder()
+
+    if net_name == 'cifar10_LeNet':
+        ae_net = CIFAR10_LeNet_Autoencoder()
+
+    if net_name == 'cifar10_LeNet_ELU':
+        ae_net = CIFAR10_LeNet_ELU_Autoencoder()
+        
+    if net_name == 'CXR_resnet18':
+        ae_net = ResNet18Autoencoder(rep_dim)
+        
+    if net_name == 'fmnist_LeNet':
+        ae_net = FMNIST_LeNet_Autoencoder()
+
+    return ae_net
